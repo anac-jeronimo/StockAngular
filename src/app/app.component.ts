3e6b440ffb11345db948996cd59a7fada481e17b
Ana Jerónimo <anac_jeronimo@hotmail.com>
2021-06-04 21:10:06 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'SockAngular';
+  title = 'StockClient';
+
+  powers = ['Power 1', 'Power 2'];
+
+  productForm = new FormGroup({
+    iva: new FormControl(''),
+    pvp: new FormControl(''),
+    discount: new FormControl(''),
+  });
+
+  productValue = '';
+
+  constructor(private http: HttpClient) {}
+
+  setProductValue(product: any) {
+    this.productValue = JSON.stringify(product);
+  }
+
+  createProduct() {
+    this.http
+      .post(
+        'http://localhost:8080/stockmanagement/api/products',
+        this.productForm.value
+      )
+      .subscribe((res: any) => this.setProductValue(res));
+  }
+
+  getAllProducts() {
+    this.http
+      .get('http://localhost:8080/stockmanagement/api/products')
+      .subscribe((res: any) => console.log(res));
+  }
 }
